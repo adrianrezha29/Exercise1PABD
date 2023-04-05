14f31f3ba818ff69a5fe0c582e224dca1dd78be5
adrianrezha29 <adrianrezha29@gmail.com>
2023-04-05 10:00:11 +0700

    add method console writeline for menampilkan menu

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -13,7 +13,19 @@ namespace Exercise1
             Program pr = new Program();
             while (true)
             {
-
+                try
+                {
+                    Console.WriteLine("Koneksi ke Database\n");
+                    Console.WriteLine("Masukkan User ID :");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan Password");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan database tujuan");
+                    string db = Console.ReadLine();
+                    Console.Write("\nKetik K untuk Terhubung ke Database: ");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                    switch (chr)
+                }
             }
         }
     }
